0036ebd3455b8d4034de1854f63e909e6fc3f4f4
marusaarticek <articek.marusa@gmail.com>
2025-12-08 08:42:44 +0100

    adding new updates

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.164/build/three.module.js";
 
+
 // --------------------------------------------------
 // DEVICE / PERFORMANCE FLAGS
 // --------------------------------------------------
